c555f2e3584dad51fe15d565f512ba66e313328c
aryzkova135-sys <aryzkova135@gmail.com>
2025-09-24 14:48:54 +0300

    Update tmp.js

--- 14-150/01/tmp.js
+++ 14-150/01/tmp.js
@@ -1,25 +1,10 @@
- function find_edit() {
-    const change_node = document.getElementById('surname');
-    change_node.innerHTML = "Ryzhkova";
+function updateNames() {
+    const surnameNode = document.getElementById('surname');
+    surnameNode.innerHTML = "Ryzhkova";
+
+    const nameNode = document.getElementById('name');
+    nameNode.innerHTML = "Anastasia";
 }
 
 const node_for_click = document.getElementById("for_click");
-node_for_click.addEventListener("click", find_edit);
-
-function find_edit() {
-    const change_node = document.getElementById('name');
-    change_node.innerHTML = "Anastasia";
-}
-
-const node_for_click = document.getElementById("for_click");
-node_for_click.addEventListener("click", find_edit);
-
-function find_edit() {
-    const change_node = document.getElementById('surname');
-    change_node.innerHTML = "Ryzhkova";
-}
-
-const node_for_click = document.getElementById("for_click");
-node_for_click.addEventListener("click", find_edit);
-
-
+node_for_click.addEventListener("click", updateNames);
