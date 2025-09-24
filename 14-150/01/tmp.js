ce6b6b2f89898fcc882899fbc959101e943899ef
aryzkova135-sys <aryzkova135@gmail.com>
2025-09-24 14:36:22 +0300

    Update tmp.js

--- 14-150/01/tmp.js
+++ 14-150/01/tmp.js
@@ -1,6 +1,6 @@
-function find_edit() {
-    const change_node = document.getElementById('name');
-    change_node.innerHTML = "Victor";
+ function find_edit() {
+    const change_node = document.getElementById('surname');
+    change_node.innerHTML = "Ryzhkova";
 }
 
 const node_for_click = document.getElementById("for_click");
